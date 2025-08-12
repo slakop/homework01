252559d6855fadd7d11df226f78ca131bcfbecf1
v_kopychko <v_kopychko@inpas.ru>
2025-08-12 18:49:14 +0300

    Выполнение домашнего задания

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -2,5 +2,21 @@ package ru.otus.homework
 
 
 fun fizzbuzz(n: Int): Array<String> {
-    TODO("Выполните задание")
+    var i = 0;
+    val numbers = Array(n, { i++}) // Создается массив чисел от 0 размерностью n
+    val result: Array<String> = Array(n) { "" } // Создается выходной массив размерностью n
+    val Fizz = "Fizz"
+    val Buzz = "Buzz"
+
+    for(i in 0..numbers.lastIndex){
+        when{
+            (numbers[i] == 0) -> result[0]=Fizz+Buzz  // 0
+            (numbers[i] % 3 == 0) && (numbers[i] % 5 == 0) -> result[i]=Fizz+Buzz // Кратно  3 и 5
+            (numbers[i] % 3 == 0) -> result[i]=Fizz   // Кратно 3
+            (numbers[i] % 5 == 0) -> result[i]=Buzz   // Кратно 5
+            else -> result[i]="${numbers[i]}"         // Все остальные значения
+        }
+    }
+
+    return result
 }
--- src/main/kotlin/ru/otus/homework/sumoftwo.kt
+++ src/main/kotlin/ru/otus/homework/sumoftwo.kt
@@ -2,5 +2,22 @@ package ru.otus.homework
 
 
 fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
-    TODO("Выполните задание")
+    var result = intArrayOf(-1) // Выходной массив
+    for(x in 0..numbers.lastIndex){
+        for(y in 0..numbers.lastIndex){
+            if ((x != y) && (numbers[x] + numbers[y] == target))
+            {
+                if (result[0] == -1) // Если массив пустой т.е. элемент -1 замещаем его
+                    result[0] = x
+                else
+                    result += x     // Если не пустой  - добавляем
+            }
+        }
+    }
+
+    // Если не нашлось элементов выкидываем исключение
+    if (result[0] == -1)
+        throw IllegalArgumentException("Array is empty")
+
+    return result
 }
